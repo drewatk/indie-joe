68839a1d8542b3e9015fb5f412d43e3b82332a23
Drew Atkinson <drewatkinson5@gmail.com>
2016-10-08 15:50:11 -0400

    layout design tweaks. Add new dropdown menu for genres

--- indie-joe/static/js/app.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $('#search_btn').on('click', function() {
-    var query =  $('#search').val()
-    $.post('/search', { query: query}, function(data) {
-      console.log(data);
-    })
-  })
-})
