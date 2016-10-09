49aab2a25e1515e50345a5fedac8adf1ba4cad08
Drew Atkinson <drewatkinson5@gmail.com>
2016-10-08 23:47:14 -0400

    even more design tweaks

---
+++ indie-joe/static/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('.center-carousel').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: false,
+        nextArrow: false,
+        pauseOnFocus: true
+    });
+});
